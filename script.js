7dfd11cce4598c36aa8fd8f19dfd05f6d7e5c7fe
madhumathi0213 <68542218+madhumathi0213@users.noreply.github.com>
2020-08-12 19:13:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function validation()
 	var pass=document.getElementById("pass").value;
 	var num=/^\d{10}$/;
 	var password=/\w/;
+	document.getElementById("errorMsg").style.visibility="hidden";
 	if((num.test(number) && password.test(pass)))
 	{
-		document.getElementById("errorMsg").style.visibility="hidden";
 		var madhu=setTimeout(myalert,100);
 	}
 	else
 	{
-		document.getElementById("errorMsg").innerHTML;
+	
 		document.getElementById("errorMsg").style.visibility="visible";	
 	}
 }
